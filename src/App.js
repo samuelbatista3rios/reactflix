5d1f10933be2634bc0889069ac94ed74d4141a1b
samuelbatista3rios <samuelbatista3rios@gmail.com>
2022-04-27 22:38:19 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import styled from 'styled-components';
+import dadosIniciais from './data/dados_iniciais.json';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
+
+const AppWrapper = styled.div`
+background:var(--grayDark);
+`;
 function App() {
   return (
-    <div>
+    <AppWrapper>
       <Menu />
-    </div>
+      <BannerMain
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoDescription={"Descrição qualquer"}
+      />
+      
+      <Carousel
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]}
+      />
+
+    </AppWrapper>
   );
 }
 
